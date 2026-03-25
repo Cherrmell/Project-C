0efa4e4be52dff8a8835f775dfde86d80c6560d1
Cherrmell <emelli.825250060@stu.untar.ac.id>
2026-03-25 19:53:36 +0700

    Create Day3_19.c

---
+++ Day3_19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int jumlahDigit(int n) {
+    if (n == 0)
+        return 0;
+    return (n % 10) + jumlahDigit(n / 10);
+}
+
+int main() {
+    int angka = 1234;
+    printf("Hasil: %d", jumlahDigit(angka));
+    return 0;
+}
