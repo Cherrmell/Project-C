39a67ea4770d80012c9a4effebd4747f6ede9b13
Cherrmell <emelli.825250060@stu.untar.ac.id>
2026-03-28 22:00:58 +0700

    Day4_30.c

---
+++ Day4_30.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, balik = 0;
+
+    printf("Masukkan angka: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        balik = balik * 10 + n % 10;
+        n /= 10;
+    }
+
+    printf("Hasil balik: %d\n", balik);
+    return 0;
+}
